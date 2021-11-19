c86eb832b905b9c846634ae65747120a481887cb
thomasc <frostu@gmail.com>
2021-11-19 14:42:53 +0100

    axe

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import React from 'react';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+if (typeof window !== 'undefined' && process.env.NODE_ENV !== 'production') {
+   const ReactDOM = require('react-dom');
+
+   const axe = require('@axe-core/react');
+
+   axe(React, ReactDOM, 1000);
 }
 
-export default MyApp
+function MyApp({ Component, pageProps }) {
+   return <Component {...pageProps} />;
+}
+
+export default MyApp;
